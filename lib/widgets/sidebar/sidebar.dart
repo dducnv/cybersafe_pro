3ba294c6ac573a76ba8e2e6bc5a066dc5b8880d2
Nguyen Van Duc <ducnv0712@gmail.com>
2025-08-03 19:09:25 +0700

    chore: Xóa gói 'macros' không còn sử dụng trong pubspec.lock và thêm import cho custom_text_style trong sidebar.dart

--- lib/widgets/sidebar/sidebar.dart
+++ lib/widgets/sidebar/sidebar.dart
@@ -6,6 +6,7 @@ import 'package:cybersafe_pro/resources/size_text_icon.dart';
 import 'package:cybersafe_pro/routes/app_routes.dart';
 import 'package:cybersafe_pro/utils/scale_utils.dart';
 import 'package:cybersafe_pro/utils/utils.dart';
+import 'package:cybersafe_pro/widgets/text_style/custom_text_style.dart';
 import 'package:flutter/material.dart';
 
 class Sidebar extends StatelessWidget {
